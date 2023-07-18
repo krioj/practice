86f5679e7409bd2fa97c23696c6bb2354a80277d
krioj <artemijkstnk@gmail.com>
2023-07-18 21:03:01 +0200

    calculator

--- practice/Program.cs
+++ practice/Program.cs
@@ -6,28 +6,28 @@ do
 
     float Namber = float.Parse(Console.ReadLine());
     float Namber2 = float.Parse(Console.ReadLine());
-    float sum = 0;
+    float resalt = 0;
     Console.WriteLine("what do you want to do? (/, *, +, -, %)");
 
     switch (Console.ReadLine())
     {
         case "/":
-            sum = Namber / Namber2;
+            resalt = Namber / Namber2;
             break;
         case "*":
-            sum = Namber * Namber2;
+            resalt = Namber * Namber2;
             break;
         case "+":
-            sum = Namber + Namber2;
+            resalt = Namber + Namber2;
             break;
         case "-":
-            sum = Namber - Namber2;
+            resalt = Namber - Namber2;
             break;
         case "%":
-            sum = Namber % Namber2;
+            resalt = Namber % Namber2;
             break;
     }
-    Console.WriteLine($"sum = {sum}");
+    Console.WriteLine($"resalt = {resalt}");
     Console.WriteLine("go out? (\"yes\" or \"no\")");
     exid = Console.ReadLine();
 } while (exid == "no");
