1b6696820817f90eccb721165ecee5080867daac
krioj <artemijkstnk@gmail.com>
2023-07-18 20:53:58 +0200

    calculator

--- practice/Program.cs
+++ practice/Program.cs
@@ -1,11 +1,36 @@
-﻿
+﻿using System.Diagnostics.CodeAnalysis;
+string exid;
+do
+{
+    Console.WriteLine("write 2 numbers");
 
+    float Namber = float.Parse(Console.ReadLine());
+    float Namber2 = float.Parse(Console.ReadLine());
+    float sum = 0;
+    Console.WriteLine("what do you want to do? (/, *, +, -, %)");
 
-
-
-
-
-
+    switch (Console.ReadLine())
+    {
+        case "/":
+            sum = Namber / Namber2;
+            break;
+        case "*":
+            sum = Namber * Namber2;
+            break;
+        case "+":
+            sum = Namber + Namber2;
+            break;
+        case "-":
+            sum = Namber - Namber2;
+            break;
+        case "%":
+            sum = Namber % Namber2;
+            break;
+    }
+    Console.WriteLine($"sum = {sum}");
+    Console.WriteLine("go out? (\"yes\" or \"no\")");
+    exid = Console.ReadLine();
+} while (exid == "no");
 
 
 
